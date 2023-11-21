93e35ac79e55026ea707da1808fd840a5ef4461d
AyalSwaid <ayalsweed5@gmail.com>
2023-11-21 21:28:22 +0200

    dashboard

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import b1Parse, b2Parse
 import pandas as pd
 
 def main():
+    # read and process xls file and convert tables into lists of tuples
     invoices = b1Parse.read_data1()
     chics_list, constant_customers_list = b2Parse.read_data2()
 
     print("Processed data")
 
+
+    # convert lists of tuples into dataframes
     invoice_df = pd.DataFrame(invoices, columns=["date", "day",
         "customer",
         "working_hours",
@@ -22,7 +25,7 @@ def main():
     cc_df = pd.DataFrame(constant_customers_list, columns=["name", "period"])
     chics_df = pd.DataFrame(chics_list, columns=["name", "date", "amount"])
 
-
+    # save data into csv files
     invoice_df.to_csv("processed_data/invoice.csv", index=False)
     cc_df.to_csv("processed_data/const_customers.csv", index=False)
     chics_df.to_csv("processed_data/chics.csv", index=False)
